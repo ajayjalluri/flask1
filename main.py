e855ec947851f97eb85ce1d20262f579325ac04b
Jalluri Ajay Vamsi <62434097+ajayjalluri@users.noreply.github.com>
2020-12-13 16:32:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
@@ -47,6 +48,24 @@ def avg():
             sum = sum +float(x)
     d = sum/len(p)
     return str(d)
+
+@app.route('/median', methods=['GET'])
+def median():
+    bar = request.args.to_dict()
+    print(bar)
+    s = bar['X']
+    p = s.split(",")
+
+    i = 0
+    for x in p :
+        if x.find('.') == -1 :
+            p[i] = int(x)
+        else :
+            p[i] = float(x)
+        i = i+1
+    d = statistics.median(p)
+    return str(d)
+
 
 if __name__ == '__main__':
     app.run(port = 80 ,debug=True)
